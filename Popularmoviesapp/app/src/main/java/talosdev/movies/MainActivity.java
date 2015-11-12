df483fd4ca97f4bb057e81c0e07d04fa0f8d3b91
Aris Papadopoulos <Aristides.PAPADOPOULOS@sword-group.com>
2015-11-12 21:26:40 +0100

    Setup project

---
+++ Popularmoviesapp/app/src/main/java/talosdev/movies/MainActivity.java
@@ -0,0 +1,17 @@
+package talosdev.movies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("TAG", "Api key is: " + BuildConfig.TMDB_API_KEY);
+
+    }
+}
