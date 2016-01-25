60cc983d3573f8892453ccc7efbd53c5b3890c26
apapad <aristides.papadopoulos@gmail.com>
2016-01-25 16:56:24 +0100

    Fixed issue with activity being recreated on back button pressed

--- Popularmoviesapp/app/src/main/java/com/talosdev/movies/ui/activity/MainActivity.java
+++ Popularmoviesapp/app/src/main/java/com/talosdev/movies/ui/activity/MainActivity.java
@@ -1,25 +1,9 @@
 package com.talosdev.movies.ui.activity;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.talosdev.movies.R;
-import com.talosdev.movies.constants.TMDB;
-import com.talosdev.movies.data.MoviePoster;
-import com.talosdev.movies.data.SortByCriterion;
-import com.talosdev.movies.remote.FetchPopularMoviesTask;
-import com.talosdev.movies.remote.json.Movie;
-import com.talosdev.movies.remote.json.MovieJSONParser;
-import com.talosdev.movies.remote.json.MovieList;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
--- Popularmoviesapp/app/src/main/java/com/talosdev/movies/ui/activity/MovieDetailActivity.java
+++ Popularmoviesapp/app/src/main/java/com/talosdev/movies/ui/activity/MovieDetailActivity.java
@@ -2,12 +2,9 @@ package com.talosdev.movies.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 
 import com.talosdev.movies.R;
 import com.talosdev.movies.constants.Intents;
-import com.talosdev.movies.remote.FetchMovieDetailsTask;
 import com.talosdev.movies.ui.MovieDetailsFragment;
 
 public class MovieDetailActivity extends Activity {
@@ -22,9 +19,5 @@ public class MovieDetailActivity extends Activity {
         long movieId = getIntent().getLongExtra(Intents.EXTRA_MOVIE_ID, 0L);
 
         fragment.setMovieId(movieId);
-
-        FetchMovieDetailsTask fetcher = new FetchMovieDetailsTask(this);
-        fetcher.execute(movieId);
-
     }
 }
