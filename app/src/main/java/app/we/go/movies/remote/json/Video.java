b36fe8298851e0f4950783e9af7de0e6d9810b97
Aris Papadopoulos <Aristides.PAPADOPOULOS@sword-group.com>
2016-03-05 14:07:16 +0100

    Trailers are persisted in all cases and not re-requested

--- app/src/main/java/app/we/go/movies/remote/json/Video.java
+++ app/src/main/java/app/we/go/movies/remote/json/Video.java
@@ -1,9 +1,12 @@
 package app.we.go.movies.remote.json;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by apapad on 2/03/16.
  */
-public class Video {
+public class Video implements Parcelable {
 
     public Video() {
     }
@@ -17,4 +20,37 @@ public class Video {
     public String name;
 
     public String type;
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.key);
+        dest.writeString(this.site);
+        dest.writeString(this.name);
+        dest.writeString(this.type);
+    }
+
+    private Video(Parcel in) {
+        this.id = in.readString();
+        this.key = in.readString();
+        this.site = in.readString();
+        this.name = in.readString();
+        this.type = in.readString();
+    }
+
+    public static final Parcelable.Creator<Video> CREATOR = new Parcelable.Creator<Video>() {
+        public Video createFromParcel(Parcel source) {
+            return new Video(source);
+        }
+
+        public Video[] newArray(int size) {
+            return new Video[size];
+        }
+    };
 }
--- app/src/main/java/app/we/go/movies/ui/tab/VideosTabFragment.java
+++ app/src/main/java/app/we/go/movies/ui/tab/VideosTabFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,20 +17,21 @@ import java.util.List;
 
 import app.we.go.movies.R;
 import app.we.go.movies.constants.Args;
-import app.we.go.movies.listener.MovieTrailerListener;
+import app.we.go.movies.listener.MovieVideosListener;
 import app.we.go.movies.remote.URLBuilder;
 import app.we.go.movies.remote.VideoAsyncLoader;
-import app.we.go.movies.remote.json.Movie;
 import app.we.go.movies.remote.json.Video;
 import hugo.weaving.DebugLog;
 
 /**
  * Created by apapad on 26/02/16.
  */
-public class VideosTabFragment extends ListFragment implements MovieTrailerListener, LoaderManager.LoaderCallbacks, AdapterView.OnItemClickListener {
+public class VideosTabFragment extends ListFragment implements MovieVideosListener, LoaderManager.LoaderCallbacks, AdapterView.OnItemClickListener {
 
 
+    private static final String BUNDLE_VIDEOS = "app.we.go.movies.BUNDLE_VIDEOS";
     private URLBuilder urlBuilder;
+    private ArrayList<Video> currentVideos;
     private long currentMovie;
 
     public static VideosTabFragment newInstance(long movieId) {
@@ -37,6 +39,7 @@ public class VideosTabFragment extends ListFragment implements MovieTrailerListe
         Bundle b = new Bundle();
         b.putLong(Args.ARG_MOVIE_ID, movieId);
         f.setArguments(b);
+        Log.d("ZZ", "Args " + movieId);
         return f;
     }
 
@@ -44,6 +47,7 @@ public class VideosTabFragment extends ListFragment implements MovieTrailerListe
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(BUNDLE_VIDEOS, currentVideos);
     }
 
     @Override
@@ -51,7 +55,6 @@ public class VideosTabFragment extends ListFragment implements MovieTrailerListe
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.movie_details_video_tab, container, false);
-
 
         return v;
     }
@@ -77,14 +80,42 @@ public class VideosTabFragment extends ListFragment implements MovieTrailerListe
         super.onActivityCreated(savedInstanceState);
         ArrayAdapter adapter = new VideoArrayAdapter(getActivity(), R.layout.video_row, new ArrayList<Video>(), getActivity().getLayoutInflater());
         setListAdapter(adapter);
-        getActivity().getSupportLoaderManager().initLoader(1, null, this).forceLoad();
+        getLoaderManager().initLoader(1, null, this);
         getListView().setOnItemClickListener(this);
 
+
+        // Restore state strategy: first check instance variable (back button), then check bundle
+        // (orientation change), and if not fetch from the network.
+        Log.d("ZZ", "Args " + getArguments().getLong(Args.ARG_MOVIE_ID) + " - currentMovie " + currentMovie);
+
+        if (currentVideos != null) {
+            // if the movie has changes, (replacing fragments in two-pane mode) we need to skip this
+            // and fetch from the network
+            if (currentMovie == getArguments().getLong(Args.ARG_MOVIE_ID)) {
+                onMovieVideosReceived(currentVideos);
+            }
+        } else if (savedInstanceState != null) {
+            ArrayList<Video> videosFromBundle = savedInstanceState.getParcelableArrayList(BUNDLE_VIDEOS);
+            if (videosFromBundle != null) {
+                onMovieVideosReceived(videosFromBundle);
+            }
+        } else {
+            getLoaderManager().getLoader(1).forceLoad();
+        }
     }
 
     @Override
-    public void onMovieTrailerReceived(Movie movie) {
+    public void onMovieVideosReceived(ArrayList<Video> videos) {
+        currentVideos = videos;
+        // currentMovie assignment must happen only here (and not in onLoadFinished)
+        currentMovie = getArguments().getLong(Args.ARG_MOVIE_ID);
+        notifyAdapter(videos);
+    }
 
+    private void notifyAdapter(ArrayList<Video> videos) {
+        currentVideos = videos;
+        ((ArrayAdapter) getListAdapter()).clear();
+        ((ArrayAdapter) getListAdapter()).addAll(videos);
     }
 
 
@@ -97,7 +128,9 @@ public class VideosTabFragment extends ListFragment implements MovieTrailerListe
 
     @Override
     public void onLoadFinished(Loader loader, Object data) {
-        ((ArrayAdapter) getListAdapter()).addAll((List<Video>) data);
+        if (data != null) {
+            notifyAdapter((ArrayList<Video>) data);
+        }
     }
 
     @Override
